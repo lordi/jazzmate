74fb0f87ecf22b4a5b11afe0e4f439b2a070c8a3
Hannes Gräuler <hgraeule@uos.de>
2011-06-19 00:02:55 +0200

    Started fiddling around mit monadic tunnels

--- jack_midi_example.hs
+++ jack_midi_example.hs
@@ -5,9 +5,31 @@ import Sound.JACK (NFrames(NFrames), )
 
 import qualified Sound.MIDI.Message as Msg
 
+import Control.Monad.State
+import Control.Monad.Reader
+import Control.Monad.Writer
+import Control.Monad.Error
+import Control.Exception
+import Data.Typeable
+import Data.IORef
+
+type NoteState = State Int
+
+updateNote :: Int -> NoteState Int
+updateNote pitch = return pitch
+
+stateioact :: StateT Int IO ()
+stateioact = do
+  x <- get
+  liftIO $ putStrLn ("stateioact called, with state as "++
+                     show x)
+  put (x+1)
 
 main :: IO ()
-main = MIDI.main foo
+main = evalStateT mainAction 42
+
+mainAction :: StateT Int IO ()
+mainAction = embedIO $ \x -> MIDI.main (makeCallback stateioact x)
 
 foo :: NFrames -> (NFrames, Msg.T) -> IO (NFrames, Msg.T)
 foo (NFrames cycleStart) (tf@(NFrames t), e) = do
@@ -15,5 +37,22 @@ foo (NFrames cycleStart) (tf@(NFrames t), e) = do
        case e of
           Msg.Channel b -> "MidiMsg.Channel " ++ show b
           Msg.System  _ -> "MidiMsg.System ..."
+    putStrLn $ "State: " ++ show ((evalState $ updateNote 3) 99)
     return (tf, e)
+
+-- the glue which makes it work:
+embedIO :: (IORef s -> IO a) -> StateT s IO a
+embedIO a = do s <- get
+               x <-  liftIO $ newIORef s
+               r <-  liftIO $ a x
+               s' <- liftIO $ readIORef x
+               put s'
+               return r
+
+makeCallback :: StateT s IO a -> IORef s -> IO a
+makeCallback act x = do s <- readIORef x
+                        (res,s') <- runStateT act s
+                        writeIORef x s'
+                        return res
+
 
