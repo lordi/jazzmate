26d4a586c6407120bb73b3c93274675e419c3cb3
Hannes Gräuler <hgraeule@uos.de>
2011-06-19 13:20:23 +0200

    State is now a list of pressed notes

--- Main.hs
+++ Main.hs
@@ -14,9 +14,11 @@ import Control.Monad.Error
 import Control.Exception
 import Data.Typeable
 import Data.IORef
+import qualified Data.List as L
 
 import Core
---type NoteCollectionT = StateT [Int] IO ()
+
+type NoteCollectionT = StateT [Int] IO ()
 
 type NoteState = State Int
 
@@ -30,23 +32,27 @@ stateioact = do
   put (x+1)
 
 
-stateioact2 :: Msg.T -> StateT Int IO ()
+stateioact2 :: Msg.T -> NoteCollectionT
 stateioact2 e = do
-    liftIO $ putStrLn $ case e of
-        Msg.Channel Ch.Cons {Ch.messageBody = (Ch.Voice (V.NoteOn pitch _))} -> "NOTE ON " ++ show pitch
-        Msg.Channel Ch.Cons {Ch.messageBody = (Ch.Voice (V.NoteOff pitch _))} -> "NOTE OFF " ++ show pitch
---        Msg.Channel b -> "MidiMsg.Channel " ++ show b
-        _ -> "MidiMsg.System ..."
     x <- get
---    liftIO $ putStrLn ("stateioact called, with state as " ++ show x)
-    put (x+1)
+--  liftIO $ putStrLn $ case e of
+--      Msg.Channel Ch.Cons {Ch.messageBody = (Ch.Voice (V.NoteOn pitch _))} -> "NOTE ON " ++ show pitch
+--      Msg.Channel Ch.Cons {Ch.messageBody = (Ch.Voice (V.NoteOff pitch _))} -> "NOTE OFF " ++ show pitch
+--        Msg.Channel b -> "MidiMsg.Channel " ++ show b
+--      _ -> "MidiMsg.System ..."
+    let y = case e of
+            Msg.Channel Ch.Cons {Ch.messageBody = (Ch.Voice (V.NoteOn pitch _))} -> ((V.fromPitch pitch):x)
+            Msg.Channel Ch.Cons {Ch.messageBody = (Ch.Voice (V.NoteOff pitch _))} -> L.delete (V.fromPitch pitch) x
+            _ -> x 
+    liftIO $ putStrLn ("stateioact called, with state as " ++ show y)
+    put y
 
 
 
 main :: IO ()
-main = evalStateT mainAction 42
+main = evalStateT mainAction []
 
-mainAction :: StateT Int IO ()
+mainAction :: NoteCollectionT
 mainAction = embedIO $ \x -> MIDI.main (makeCallback2 stateioact2 x)
 
 -- the glue which makes it work:
@@ -58,11 +64,11 @@ embedIO a = do s <- get
                put s'
                return r
 
-makeCallback2 :: (Msg.T -> StateT s IO a) -> IORef s -> NFrames -> (NFrames, Msg.T) -> IO (NFrames, Msg.T)
+makeCallback2 :: (Msg.T -> NoteCollectionT) -> IORef [Int] -> NFrames -> (NFrames, Msg.T) -> IO (NFrames, Msg.T)
 makeCallback2 act x (NFrames cycleStart) (tf@(NFrames t), e) = do 
                         s <- readIORef x
-                        (res,s') <- runStateT (act e) s
-                        writeIORef x s'
+                        (_,sy) <- runStateT (act e) s
+                        writeIORef x sy
                         return (tf, e)
 
 
