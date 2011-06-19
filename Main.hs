29a24ab96626538d0e58e6404867ec5af21f3dca
Hannes Gräuler <hgraeule@uos.de>
2011-06-19 16:34:59 +0200

    Cleaned up Main module (still pretty obscure)

--- Main.hs
+++ Main.hs
@@ -1,10 +1,11 @@
 module Main where
 
+import qualified Data.List as L
+
 import qualified Sound.JACK.MIDI as MIDI
 import Sound.JACK (NFrames(NFrames), )
-
 import qualified Sound.MIDI.Message as Msg
-import qualified Sound.MIDI.Message.Channel as Ch
+import Sound.MIDI.Message.Channel (T (Cons), messageBody, Body (Voice))
 import qualified Sound.MIDI.Message.Channel.Voice as V
 
 import Control.Monad.State
@@ -14,32 +15,39 @@ import Control.Monad.Error
 import Control.Exception
 import Data.Typeable
 import Data.IORef
-import qualified Data.List as L
 
 import Core
 
-type NoteCollectionT = StateT [Key] IO ()
+type KeyCollectionT = StateT [Key] IO ()
 
 fromPitch :: V.Pitch -> Key
 fromPitch p = toEnum ((V.fromPitch p) `mod` octave)
 
-stateioact2 :: Msg.T -> NoteCollectionT
-stateioact2 e = do
-    x <- get
-    let y = case e of
-            Msg.Channel Ch.Cons {Ch.messageBody = (Ch.Voice (V.NoteOn pitch _))} -> ((fromPitch pitch):x)
-            Msg.Channel Ch.Cons {Ch.messageBody = (Ch.Voice (V.NoteOff pitch _))} -> L.delete (fromPitch pitch) x
-            _ -> x 
-    liftIO $ putStrLn ("\n" ++ show y ++ "\n" ++ show (intervals y) ++ "\n" ++ show (matchingChords y))
-    liftIO $ ppkeys (Notes y)
-    put y
+transform :: Msg.T -> [Key] -> [Key]
+transform (Msg.Channel Cons {messageBody = (Voice (V.NoteOn p _))})
+            = (:) $ fromPitch p
+transform (Msg.Channel Cons {messageBody = (Voice (V.NoteOff p _))})
+            = L.delete $ fromPitch p
+transform _ = id
+
+updateColl :: Msg.T -> KeyCollectionT
+updateColl e = do x <- get; put $ transform e x; printColl
+
+printColl :: KeyCollectionT
+printColl = do
+    y <- get
+    liftIO $ putStrLn $ show y
+    liftIO $ putStrLn $ show (intervals y)
+    liftIO $ putStrLn $ show (matchingChords y)
+    liftIO $ ppkeys $ Notes y
 
 main :: IO ()
 main = evalStateT mainAction []
 
-mainAction :: NoteCollectionT
-mainAction = embedIO $ \x -> MIDI.main (makeCallback2 stateioact2 x)
+mainAction :: KeyCollectionT
+mainAction = embedIO $ \x -> MIDI.main (makeCallback updateColl x)
 
+-- from http://www.haskell.org/pipermail/haskell-cafe/2007-July/028501.html
 -- the glue which makes it work:
 embedIO :: (IORef s -> IO a) -> StateT s IO a
 embedIO a = do s <- get
@@ -49,8 +57,9 @@ embedIO a = do s <- get
                put s'
                return r
 
-makeCallback2 :: (Msg.T -> NoteCollectionT) -> IORef [Key] -> NFrames -> (NFrames, Msg.T) -> IO (NFrames, Msg.T)
-makeCallback2 act x (NFrames cycleStart) (tf@(NFrames t), e) = do 
+makeCallback :: (Msg.T -> KeyCollectionT) -> IORef [Key] ->
+                NFrames -> (NFrames, Msg.T) -> IO (NFrames, Msg.T)
+makeCallback act x _ (tf@(NFrames t), e) = do
                         s <- readIORef x
                         (_,sy) <- runStateT (act e) s
                         writeIORef x sy
