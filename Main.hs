7c1243daf67131a014e1ac518eb8cf3b688a2e20
Hannes Gräuler <hgraeule@uos.de>
2011-06-19 12:53:17 +0200

    Successfully tunneled monads: Program prints MIDI signals and keeps a state.

--- Main.hs
+++ Main.hs
@@ -4,6 +4,8 @@ import qualified Sound.JACK.MIDI as MIDI
 import Sound.JACK (NFrames(NFrames), )
 
 import qualified Sound.MIDI.Message as Msg
+import qualified Sound.MIDI.Message.Channel as Ch
+import qualified Sound.MIDI.Message.Channel.Voice as V
 
 import Control.Monad.State
 import Control.Monad.Reader
@@ -14,6 +16,7 @@ import Data.Typeable
 import Data.IORef
 
 import Core
+--type NoteCollectionT = StateT [Int] IO ()
 
 type NoteState = State Int
 
@@ -26,11 +29,25 @@ stateioact = do
   liftIO $ putStrLn ("stateioact called, with state as " ++ show x)
   put (x+1)
 
+
+stateioact2 :: Msg.T -> StateT Int IO ()
+stateioact2 e = do
+    liftIO $ putStrLn $ case e of
+        Msg.Channel Ch.Cons {Ch.messageBody = (Ch.Voice (V.NoteOn pitch _))} -> "NOTE ON " ++ show pitch
+        Msg.Channel Ch.Cons {Ch.messageBody = (Ch.Voice (V.NoteOff pitch _))} -> "NOTE OFF " ++ show pitch
+--        Msg.Channel b -> "MidiMsg.Channel " ++ show b
+        _ -> "MidiMsg.System ..."
+    x <- get
+--    liftIO $ putStrLn ("stateioact called, with state as " ++ show x)
+    put (x+1)
+
+
+
 main :: IO ()
 main = evalStateT mainAction 42
 
 mainAction :: StateT Int IO ()
-mainAction = embedIO $ \x -> MIDI.main (makeCallback2 stateioact x)
+mainAction = embedIO $ \x -> MIDI.main (makeCallback2 stateioact2 x)
 
 -- the glue which makes it work:
 embedIO :: (IORef s -> IO a) -> StateT s IO a
@@ -41,15 +58,10 @@ embedIO a = do s <- get
                put s'
                return r
 
-makeCallback2 :: StateT s IO a -> IORef s -> NFrames -> (NFrames, Msg.T) -> IO (NFrames, Msg.T)
+makeCallback2 :: (Msg.T -> StateT s IO a) -> IORef s -> NFrames -> (NFrames, Msg.T) -> IO (NFrames, Msg.T)
 makeCallback2 act x (NFrames cycleStart) (tf@(NFrames t), e) = do 
-                        putStrLn $ "Time: " ++ show (cycleStart + t) ++ " " ++
-                           case e of
-                              Msg.Channel b -> "MidiMsg.Channel " ++ show b
-                              Msg.System  _ -> "MidiMsg.System ..."
-                        putStrLn $ "State: " ++ show ((evalState $ updateNote 3) 99)
                         s <- readIORef x
-                        (res,s') <- runStateT act s
+                        (res,s') <- runStateT (act e) s
                         writeIORef x s'
                         return (tf, e)
 
