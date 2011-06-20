364fc2723d4e4bb69de49390e3d9b712c9d19dd3
Hannes Gräuler <hgraeule@uos.de>
2011-06-20 21:16:46 +0200

    Avoid do notation in transformC

--- Main.hs
+++ Main.hs
@@ -20,9 +20,13 @@ import Core
 
 type KeyCollectionT = StateT [Key] IO ()
 
+-- | Converts a MIDI pitch to an element from the Key enum defined in Core
 fromPitch :: V.Pitch -> Key
 fromPitch p = toEnum ((V.fromPitch p) `mod` octave)
 
+-- | The core of this module's functionality: Take a MIDI message and a list
+-- of keys, and return the resulting list. Pressing a key will add the
+-- corresponding note to the list, lifting it will delete it.
 transform :: Msg.T -> [Key] -> [Key]
 transform (Msg.Channel Cons {messageBody = (Voice (V.NoteOn p _))})
             = (:) $ fromPitch p
@@ -30,22 +34,20 @@ transform (Msg.Channel Cons {messageBody = (Voice (V.NoteOff p _))})
             = L.delete $ fromPitch p
 transform _ = id
 
-updateColl :: Msg.T -> KeyCollectionT
-updateColl e = do x <- get; put $ transform e x; printColl
-
-printColl :: KeyCollectionT
-printColl = do
-    y <- get
-    liftIO $ putStrLn $ show y
-    liftIO $ putStrLn $ show (intervals y)
-    liftIO $ putStrLn $ show (matchingChords y)
-    liftIO $ ppkeys $ Notes y
+transformC :: Msg.T -> KeyCollectionT
+transformC e = get >>= \x -> put (transform e x)
 
 main :: IO ()
 main = evalStateT mainAction []
 
 mainAction :: KeyCollectionT
-mainAction = embedIO $ \x -> MIDI.main (makeCallback updateColl x)
+mainAction = embedIO $ \x -> MIDI.main (makeCallback (transformC >> printC) x)
+    where printC e = do
+                     y <- get
+                     liftIO $ putStrLn $ show y
+                     liftIO $ putStrLn $ show (intervals y)
+                     liftIO $ putStrLn $ show (matchingChords y)
+                     liftIO $ ppkeys $ Notes y
 
 -- from http://www.haskell.org/pipermail/haskell-cafe/2007-July/028501.html
 -- the glue which makes it work:
