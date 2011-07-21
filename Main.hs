4a33681e7d3d39399251011d7542acec9ec2691b
Hannes Gräuler <hgraeule@uos.de>
2011-07-21 21:56:02 +0200

    First attempt to render a piano keyboard to the screen.

--- Main.hs
+++ Main.hs
@@ -11,7 +11,7 @@ import Data.Typeable
 import Data.IORef
 
 import Graphics.UI.Gtk hiding(get)
-import Graphics.Rendering.Cairo hiding(scale,transform)
+import qualified Graphics.Rendering.Cairo as C
 import Control.Concurrent
 
 import Core
@@ -19,13 +19,54 @@ import qualified MIDIBridge
 
 import Data.Maybe
 
+foreach :: (Monad m) => [a] -> (a -> m b) -> m [b]
+foreach = flip mapM
+
+-- | First try to write a function that renders a piano on the screen. Still
+-- very ugly duckling, needs rewrite.
+renderKeyboard keys (w, h) = do
+    let keysize = realToFrac w / 7
+        blackheight = realToFrac h * 2 / 3
+        br1 = [Db,Eb]
+        br2 = [Gb,Ab,Bb]
+        wr = [C,D,E,F,G,A,B]
+        whitecolor key = if (key `elem` keys) then 1.0 else 0.5
+        blackcolor key = if (key `elem` keys) then 1.0 else 0.0
+        drawKey x y w_ h_ c = do
+                                C.moveTo x y
+                                C.lineTo (x + w_) y
+                                C.lineTo (x + w_) (y + h_)
+                                C.lineTo x (y + h_)
+                                C.closePath
+                                C.setSourceRGBA 1.0 1.0 c 1.0
+                                C.fillPreserve
+                                C.setSourceRGBA 0.0 0.0 0.0 1.0
+                                C.stroke
+ 
+    C.setLineCap C.LineCapRound
+    C.setLineJoin C.LineJoinRound
+    C.setLineWidth 2
+
+    foreach ([0 .. 6] :: [Int]) $ \ x ->
+        do let col = whitecolor $ wr !! x
+           drawKey (realToFrac x * keysize) 0 keysize h col
+
+    foreach ([0 .. 1] :: [Int]) $ \ x ->
+        do let col = blackcolor $ br1 !! x
+               shiftx = keysize / 2
+           drawKey (realToFrac x * keysize + shiftx) 0 keysize blackheight col
+
+    foreach ([0 .. 2] :: [Int]) $ \ x ->
+        do let col = blackcolor $ br2 !! x
+               shiftx = keysize * 3 + keysize / 2
+           drawKey (realToFrac x * keysize + shiftx) 0 keysize blackheight col
+
+
 renderCanvas st (w, h) = do
-    setSourceRGBA 0 0 0 0.5
-    setLineWidth 2.5
-    stroke
-    moveTo 10 10
-    lineTo 20 20
-    showText $ "Current keys: " ++ (show st) ++ "; Canvas size: " ++ (show (w, h))
+    C.setSourceRGBA 0 0 0 1.0
+    C.moveTo 10 250
+    C.showText $ "Current keys: " ++ (show st) ++ "; Canvas size: " ++ (show (w, h))
+    renderKeyboard st (300, 200)
 
 invalidate :: DrawingArea -> IO ()
 invalidate win = do
